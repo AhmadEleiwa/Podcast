8068eb2bb8cff6d84507c4428371f4ee5b049509
ahmadeleiwa <ahmadilawa@gmail.com>
2023-05-10 12:28:06 +0300

    Adding param to component tom make it much esier in the outside

--- src/shared/Link/index.js
+++ src/shared/Link/index.js
@@ -1,6 +1,15 @@
 import { Link as RouteLink } from 'react-router-dom'
 import style from './style.module.css'
+import { CSSProperties } from 'react'
 
+/**
+ * 
+ * @param {{
+ * to: String
+ * style: CSSProperties
+ * }} props 
+ * @returns Link
+ */
 
 const Link = props => {
 
