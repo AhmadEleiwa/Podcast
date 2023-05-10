db1d3735a3367d216cf9643f0d97f15ea5c4b862
ahmadeleiwa <ahmadilawa@gmail.com>
2023-05-10 12:21:47 +0300

    Allow media property

--- src/shared/Card/index.js
+++ src/shared/Card/index.js
@@ -1,20 +1,46 @@
 import { CardContent, CardMedia, Card as MUICard, Typography } from "@mui/material"
 
 import style from './style.module.css'
+import { Link } from "react-router-dom"
 
+/**
+ * @param {{
+ * to:String,
+ * media:String,
+ * title: String,
+ * genre: String}} props 
+ * @returns Link / MUICard
+ */
 const Card = props => {
-    return <MUICard className={style.card} >
-        <CardMedia component={'img'} src="https://picsum.photos/200/300" />
-        <CardContent className={style.cardContent}>
-            <Typography varint='h2'  component={'h2'}  >
-                {props.title}
-            </Typography>
-            <Typography component={'p'} variant="p" >
-                {props.genre}
-            </Typography>
-        </CardContent>
-    </MUICard>
+    return <>
+        {props.to
+            ? <Link to={props.to}>
+                <MUICard className={style.card} >
+                    <CardMedia component={'img'} src={props.media} />
+                    <CardContent className={style.cardContent}>
+                        <Typography varint='h2' component={'h2'}  >
+                            {props.title}
+                        </Typography>
+                        <Typography component={'p'} variant="p" >
+                            {props.genre}
+                        </Typography>
+                    </CardContent>
+                </MUICard>
+            </Link > // when to property not null 
 
+            : <MUICard className={style.card} >
+                <CardMedia component={'img'} src={"https://picsum.photos/200/300"} />
+                <CardContent className={style.cardContent}>
+                    <Typography varint='h2' component={'h2'}  >
+                        {props.title}
+                    </Typography>
+                    <Typography component={'p'} variant="p" >
+                        {props.genre}
+                    </Typography>
+                </CardContent>
+            </MUICard>
+        }
+    </>
 }
 
 export default Card
