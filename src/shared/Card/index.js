812bdd0ada73a9b52cf179370d97d3a1e781247d
ahmadeleiwa <ahmadilawa@gmail.com>
2023-05-11 00:12:41 +0300

    Remove trenary operator

--- src/shared/Card/index.js
+++ src/shared/Card/index.js
@@ -13,35 +13,21 @@ import { Link } from "react-router-dom"
  * @returns Link / MUICard
  */
 const Card = props => {
-    return <>
-        {props.to
-            ? <Link to={props.to}>
-                <MUICard className={style.card} >
-                    <CardMedia component={'img'} src={props.media} />
-                    <CardContent className={style.cardContent}>
-                        <Typography varint='h2' component={'h2'}  >
-                            {props.title}
-                        </Typography>
-                        <Typography component={'p'} variant="p" >
-                            {props.genre}
-                        </Typography>
-                    </CardContent>
-                </MUICard>
-            </Link > // when to property not null 
+    return <Link to={props.to}>
+        <MUICard className={style.card} >
+            <CardMedia component={'img'} src={props.media} />
+            <CardContent className={style.cardContent}>
+                <Typography varint='h2' component={'h2'}  >
+                    {props.title}
+                </Typography>
+                <Typography component={'p'} variant="p" >
+                    {props.genre}
+                </Typography>
+            </CardContent>
+        </MUICard>
+    </Link > // when to property not null 
 
-            : <MUICard className={style.card} >
-                <CardMedia component={'img'} src={"https://picsum.photos/200/300"} />
-                <CardContent className={style.cardContent}>
-                    <Typography varint='h2' component={'h2'}  >
-                        {props.title}
-                    </Typography>
-                    <Typography component={'p'} variant="p" >
-                        {props.genre}
-                    </Typography>
-                </CardContent>
-            </MUICard>
-        }
-    </>
+
 }
 
 export default Card
