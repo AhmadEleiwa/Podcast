2b42e2e798721928b667641dcd8b52a01e956343
ahmadeleiwa <ahmadilawa@gmail.com>
2023-05-16 00:35:50 +0300

    Give onClick props as props

--- src/shared/Button/index.js
+++ src/shared/Button/index.js
@@ -1,11 +1,21 @@
-import { Button as MUIButton } from "@mui/material"
+import { Button as MUIButton } from "@mui/material";
 /**
- * 
- * @param {{color:String}} props 
- * @returns 
+ *
+ * @param {{color:String, onClick:()=>{}}} props
+ * @returns
  */
-const Button = props=>{
-    return <MUIButton variant="outlined" sx={{borderRadius:'50em'}} color={props.color?props.color:'primary'}>test</MUIButton>
-}
+const Button = (props) => {
+  return (
+    <MUIButton
+        
+      variant="outlined"
+      onClick={props.onClick}
+      sx={{ borderRadius: "50em !important" }}
+      color={props.color ? props.color : "primary"}
+    >
+      {props.children}
+    </MUIButton>
+  );
+};
 
-export default Button
+export default Button;
