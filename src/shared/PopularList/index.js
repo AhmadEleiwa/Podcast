395d84219b0ba0fd57eed3b37b2805886c1e0f40
ahmadeleiwa <ahmadilawa@gmail.com>
2023-05-28 23:27:14 +0300

    Edit the dummy data to fill of the content

--- src/shared/PopularList/index.js
+++ src/shared/PopularList/index.js
@@ -15,37 +15,37 @@ const PopularList = (props) => {
       {
         id: "14w",
         title: "Et omnis aut id",
-        description: "Et omnis aut id. Aut eaque est quaerat. ",
+        description: "Et omnis aut id. Aut eaque est quaerat. s aut id. Aut  est qu    ",
         src: "https://picsum.photos/200/300",
       },
       {
         id: "14a",
         title: "Et omnis aut id",
-        description: "Et omnis aut id. Aut eaque est quaerat. ",
+        description: "Et omnis aut id. Aut eaque est quaerat. aut id. Aut eaque  aque ",
         src: "https://picsum.photos/200/300",
       },
       {
         id: "14s",
         title: "Et omnis aut id",
-        description: "Et omnis aut id. Aut eaque est quaerat. ",
+        description: "Et omnis aut id. Auerat. omnis  id. Auera id. Auera t. omnis    ",
         src: "https://picsum.photos/200/300",
       },
       {
         id: "144",
         title: "Et omnis aut id",
-        description: "Et omnis aut id. Aut eaque est quaerat. ",
+        description: "Et omnis aut id. Aut eaque est quaerat. ut eaque est quaerat ",
         src: "https://picsum.photos/200/300",
       },
       {
         id: "878",
         title: "Et omnis aut id",
-        description: "Et omnis aut id. Aut eaque est quaerat. ",
+        description: "Et omnis aut id. Aut eaque est quaerat. id. Aut eaque est quaera ",
         src: "https://picsum.photos/200/300",
       },
     ]);
   }, []);
   return (
-    <Box maxWidth={"30em"} display={"grid"} gap={4}>
+    <Box display={"grid"} gap={4}>
         <Typography variant="h3" fontSize={'32px'}>
         Popular at the 
        <MaskedText>moment</MaskedText>
