a9c312d6de692da62ed1c84df811d19cf300666d
ahmadeleiwa <ahmadilawa@gmail.com>
2023-05-28 14:39:25 +0300

    Retrive the last changes in Router.js

---
+++ src/Router.js
@@ -0,0 +1,34 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Layout from "./Layout";
+
+const Router = (props) => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          index
+          path="/"
+          element={
+            <Layout minimumHeader={false}>
+              <Home />
+            </Layout>
+          }
+        ></Route>
+        {/* home */}
+
+        <Route path="/pricing"></Route>
+        {/* Explore - Need Wrapping with Guard */}
+
+        <Route path="/dashbord"></Route>
+        {/* Explore - Need Wrapping with Guard */}
+
+        <Route path="/login" element={<AuthPage login />}></Route>
+        <Route path="/signup" element={<AuthPage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
