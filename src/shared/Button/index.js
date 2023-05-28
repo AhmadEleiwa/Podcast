4db58ae9dd0c600350dab81578e638a6dbc3ab2d
ahmadeleiwa <ahmadilawa@gmail.com>
2023-05-28 22:52:47 +0300

    Add fullWidth? prop

--- src/shared/Button/index.js
+++ src/shared/Button/index.js
@@ -1,7 +1,7 @@
 import { Button as MUIButton } from "@mui/material";
 /**
  *
- * @param {{color:String, variant: "outlined" | "contained", type:'button' | 'submit'}} props
+ * @param {{color:String, variant: "outlined" | "contained", type:'button' | 'submit', fullWidth:boolean}} props
  * @returns
  */
 const Button = (props) => {
@@ -12,6 +12,7 @@ const Button = (props) => {
       color={props.color ? props.color : "primary"}
       onClick={props.onClick}
       type={props.type}
+      fullWidth={props.fullWidth}
     >
       {props.children}
     </MUIButton>
