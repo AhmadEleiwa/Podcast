80a54f3ab56564c1428f33595dda9deaf3aae2c8
ahmadeleiwa <ahmadilawa@gmail.com>
2023-05-31 11:15:04 +0300

    Resolve the conflict

--- src/Router.js
+++ src/Router.js
@@ -1,26 +1,80 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const Router = props => {
-    return <BrowserRouter>
-        <Routes>
-            <Route index path='/' element={<h1>test</h1>}></Route>
-            {/* home */}
+import Home from "./pages/Home";
+import Layout from "./Layout";
+import Dashboard from "./pages/Dashboard";
+import AuthPage from "./pages/Auth";
+import Guard, { LoginGuard } from "./Guards";
+import Logout from "./pages/Logout";
+import Explore from "./pages/Explore";
+import Player from "./shared/Player";
+import Pricing from "./pages/Pricing";
+const Router = (props) => {
+  return (
+    <BrowserRouter>
+      <Player />
+      <Routes>
+        <Route
+          index
+          path="/"
+          element={
+            <Layout minimumHeader={false}>
+              <Home />
+            </Layout>
+          }
+        ></Route>
+        {/* home */}
+        <Route path="/logout" element={<Logout />}></Route>
 
-            <Route path='/Explore'></Route>
-            {/* Explore - Need Wrapping with Guard */}
+        <Route
+          path="/dashboard"
+          element={
+            <Guard>
+              <Dashboard />
+            </Guard>
+          }
+        ></Route>
 
-            <Route path='/pricing'></Route>
-            {/* Explore - Need Wrapping with Guard */}
-
-            <Route path='/dashbord'></Route>
-            {/* Explore - Need Wrapping with Guard */}
-
-            <Route path='login'></Route>
-            <Route path='signup'></Route>
-
-
-        </Routes>
+        <Route
+          path="/explore"
+          element={
+            <Guard>
+              <Layout minimumHeader={true}>
+                <Explore />
+              </Layout>
+            </Guard>
+          }
+        ></Route>
+        {/* Explore - Need Wrapping with Guard */}
+        <Route
+          path="/pricing"
+          element={
+            <Guard>
+              <Layout minimumHeader={true}>
+                <Pricing />
+              </Layout>
+            </Guard>
+          }
+        ></Route>
+        <Route
+          path="/login"
+          element={
+            <LoginGuard>
+              <AuthPage login />
+            </LoginGuard>
+          }
+        ></Route>
+        <Route
+          path="/signup"
+          element={
+            <LoginGuard>
+              <AuthPage />
+            </LoginGuard>
+          }
+        ></Route>
+      </Routes>
     </BrowserRouter>
-}
+  );
+};
 
-export default Router
+export default Router;
